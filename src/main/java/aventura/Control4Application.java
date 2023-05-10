a9f1e25896ed29a353899c30df955f25ad731d58
Júlio Cesar Melegari <juliocesar.melegari@gmail.com>
2023-05-10 11:17:43 -0300

    Project created

---
+++ src/main/java/aventura/Control4Application.java
@@ -0,0 +1,13 @@
+package aventura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Control4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Control4Application.class, args);
+	}
+
+}
---
+++ src/test/java/aventura/Control4ApplicationTests.java
@@ -0,0 +1,13 @@
+package aventura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Control4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
